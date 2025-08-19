ad06f171ade57248fb1ce95df2b3dd6373810684
Anan Hamdy <nonahamdy205@gmail.com>
2025-08-20 01:52:54 +0300

    create home details section & home products section

---
+++ home/js/script.js
@@ -0,0 +1,88 @@
+let homeProducts = [
+    {
+      id: 1,
+      name: "Clove Console Table",
+      price: "700",
+      oldPrice: null,
+      image: "https://thefurnituregallery.com.au/cdn/shop/files/G1A4277_800x.jpg?v=1752558496"
+    },
+    {
+      id: 2,
+      name: "Seville Nest of Coffee Tables",
+      price: "899",
+      oldPrice: "$1599",
+      image: "https://thefurnituregallery.com.au/cdn/shop/files/G1A4111_800x.jpg?v=1754356774"
+    },
+    {
+      id: 3,
+      name: "Reeves 6 Seater with Ottoman - Exclusive Cobblestone",
+      price: "6198",
+      oldPrice: "$10296",
+      image: "https://thefurnituregallery.com.au/cdn/shop/files/Hero_5310cd76-5b6b-4368-9f9d-ce5ebc820ba5_800x.png?v=1753264119"
+    },
+    {
+      id: 4,
+      name: "Allure Bed with Drawers",
+      price: "1399",
+      oldPrice: "$1999",
+      image: "https://thefurnituregallery.com.au/cdn/shop/files/G1A3666copy_800x.jpg?v=1753456616"
+    },
+    {
+      id: 5,
+      name: "Coco Dining Chair",
+      price: "299",
+      oldPrice: "$399",
+      image: "https://thefurnituregallery.com.au/cdn/shop/files/G1A4176_3_800x.jpg?v=1752558760"
+    },
+    {
+      id: 6,
+      name: "Juni 2400 Dining Table - White",
+      price: "3699",
+      oldPrice: "$7699",
+      image: "https://thefurnituregallery.com.au/cdn/shop/files/IMG_7996_800x.jpg?v=1752561036"
+    },
+    {
+      id: 7,
+      name: "Nolan Bedside Table - Oak",
+      price: "499",
+      oldPrice:"$599",
+      image: "https://thefurnituregallery.com.au/cdn/shop/files/G1A3791_800x.jpg?v=1752711759"
+    },
+    {
+      id: 8,
+      name: "Florabelle Arm Chair - Cookie",
+      price: "1699",
+      oldPrice: "$2399",
+      image: "https://thefurnituregallery.com.au/cdn/shop/files/G1A3826_800x.jpg?v=1753852623"
+    },
+
+  ];
+
+localStorage.setItem("homeprouducts",JSON.stringify(homeProducts)); 
+
+let productList = document.getElementById("product-list");
+
+let products = JSON.parse(localStorage.getItem("homeprouducts"));
+
+products.forEach(p => {
+  let card = document.createElement("div");
+  card.className = "col-md-3 mb-4";
+
+  card.innerHTML = `
+    <div class="card product-card ">
+      <img src="${p.image}" class="card-img-top" alt="${p.name}">
+      <div class="card-body">
+        <h6 class="card-title text-start">${p.name}</h6>
+        <p class="card-text text-start">
+          <span class="newprice ">$${p.price}</span>
+          ${p.oldPrice ? `<span class="old-price ms-2">${p.oldPrice}</span>` : ""}
+        </p>
+      </div>
+    </div>
+  `;
+   card.addEventListener("click", () => {
+    window.location.href = `#${p.id}`;
+  });
+
+  productList.appendChild(card);
+});
