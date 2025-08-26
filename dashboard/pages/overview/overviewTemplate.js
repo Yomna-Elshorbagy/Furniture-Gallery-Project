f7a2c6c2d224f1ea0a846c0276b7966c991f0274
Eman Zyada <eman.a.ziada@gmail.com>
2025-08-27 00:23:22 +0300

    overview

--- dashboard/pages/overview/overviewTemplate.js
+++ dashboard/pages/overview/overviewTemplate.js
@@ -1,6 +1,9 @@
 export const overviewTemplate = `
-    <canvas id="TotalPriceChart" style="width:100%;max-width:600px"></canvas>
-    <div id="myPlot" style="width:100%;max-width:700px"></div>
-    <canvas id="Status" style="width:100%;max-width:600px"></canvas>
+<div id="myPlot" style="width:100%;max-width:500px"></div>
+<div class="row">
+    <canvas class="col-6" id="TotalPriceChart" style="width:100%;max-width:600px"></canvas>
+    <canvas  class="col-6" id="Status" style="width:100%;max-width:600px;display:inline-block"></canvas>
+</div>
+
 
 `
