9a5884dc162a3f0372f186eb86afbe507aec327f
Yomna Elshorbagy <yumnamohamed30@gmail.com>
2025-09-02 14:04:07 +0300

    adding sign up with google auth

--- Auth/sign-up/script.js
+++ Auth/sign-up/script.js
@@ -210,3 +210,60 @@ document.addEventListener("click", (e) => {
     window.location.href = link.href;
   }
 });
+// ===== GOOGLE SIGNUP HANDLER =====
+const googleBtn = document.querySelector(".google-btn");
+
+if (googleBtn) {
+  googleBtn.addEventListener("click", () => {
+    google.accounts.id.initialize({
+      client_id:
+        "700704531343-a9a4dtrmun32cn8g9vaq0geckdlrdhf9.apps.googleusercontent.com",
+      callback: handleGoogleResponse,
+    });
+
+    google.accounts.id.prompt(); // opens the popup for errors
+  });
+}
+
+// Process Google login response
+function handleGoogleResponse(response) {
+  // Decode JWT
+  const data = JSON.parse(atob(response.credential.split(".")[1]));
+  console.log("Google user:", data);
+
+  // Check if user already exists
+  let users = getUsers();
+  let existingUser = users.find(
+    (u) => u.Email.toLowerCase() === data.email.toLowerCase()
+  );
+
+  let loggedInUser;
+  if (!existingUser) {
+    let id = Date.now() + Math.floor(Math.random() * 1000);
+    loggedInUser = new User(
+      id,
+      data.name,
+      data.email,
+      data.sub, // save google ID as "phone" just to keep schema filled
+      "google-auth", // this is default password but not needed
+      "user"
+    );
+    users.push(loggedInUser);
+    saveUsers(users);
+  } else {
+    loggedInUser = existingUser;
+  }
+
+  // Save session
+  localStorage.setItem("loggedInUser", JSON.stringify(loggedInUser));
+
+  Swal.fire({
+    title: `🎉 Welcome ${data.name}!`,
+    text: "You have signed up with Google successfully.",
+    icon: "success",
+    timer: 2000,
+    showConfirmButton: false,
+  }).then(() => {
+    window.location.href = "../../home/home.html";
+  });
+}
